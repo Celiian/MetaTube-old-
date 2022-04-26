8a965c20911080e0f10a8c4bc9dace08ce03efcb
Killian Vendewinkele <killianvendewinkele@MacBook-Pro-de-admin.local>
2022-04-26 08:23:02 +0200

    feature/ search page created, filter started

---
+++ src/components/SearchPage.vue
@@ -0,0 +1,38 @@
+<script>
+    import filters from './../partials/Filters.vue'
+    export default {
+        components: { filters },
+        name: "SearchPage",
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    function displayFilter() {
+        if(document.getElementById('filter-box').style.display == 'none'){document.getElementById('filter-box').style.display = 'block'}else{
+                    document.getElementById('filter-box').style.display = 'none'
+                }
+    }
+</script>
+
+<template>
+    <filters></filters>
+</template>
+
+
+<style>
+    .row{
+        display: flex;
+        flex-direction: row;
+    }
+    .column{
+        display: flex;
+        flex-direction: column;
+    }
+    #filter-box{
+        display : none;
+    }
+    #filter-box:hover{
+        display : block;
+    }
+
+</style>
+
---
+++ src/partials/Filters.vue
@@ -0,0 +1,45 @@
+<template>
+<section>
+        <button type="button" @click="displayfilter" id="filter-button">Filtres</button>
+        <div id="filter-box" class="row">
+            <div class="column">
+                <h3>Date d'ajout</h3>
+                <a onclick="">Dernière heure</a>
+                <a onclick="">Aujourd'hui</a>
+                <a onclick="">Cette semaine</a>
+                <a onclick="">Ce mois-ci</a>
+                <a onclick="">Cette année</a>
+            </div>
+            <div class="column">
+                <h3>Type</h3>
+                <a onclick="">Vidéo</a>
+                <a onclick="">Chaîne</a>
+                <a onclick="">Playlist</a>
+            </div>
+            <div class="column">
+                <h3>Trier par</h3>
+                <a onclick="">Pertinence</a>
+                <a onclick="">Date de mise en ligne</a>
+                <a onclick="">Nombre de vues</a>
+                <a onclick="">Avis</a>
+            </div>
+        </div>
+    </section>
+    <section class="column">
+        <div class="row">
+            <a href=""><img src=""></a>
+            <div>
+                <h3>Titre de vidéo</h3>
+                <p>Nombre de vues</p>
+                <p>Description</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "filters",
+    }
+    
+</script>
