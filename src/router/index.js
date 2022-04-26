9dd15fc12f98ad15eb44b5e3d280a0cd02477617
elimf <elim.florvil@gmail.com>
2022-04-26 09:50:14 +0200

    Feature/Register

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,30 @@
 /* eslint-disable */
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Register from '../views/Register.vue'
-import Home from '../views/Home.vue'
-
+import { createRouter, createWebHashHistory } from "vue-router";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
 
 const routes = [
-  {
-    path: '/',
-    name:'home',
-    component: Home
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
-  
-]
+    {
+        path: "/",
+        name: "home",
+        component: Home,
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: Register,
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login,
+    },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+    history: createWebHashHistory(),
+    routes,
+});
 
-export default router
+export default router;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,99 +4,59 @@
             <div class="shape"></div>
             <div class="shape"></div>
         </div>
-        <form>
-            <h3>Login Here</h3>
-            <div id="img-preview"></div>
-            <input
-                type="file"
-                id="choose-file"
-                name="choose-file"
-                accept="image/*"
-            />
-            <label for="choose-file">Choose File</label>
+       
+        <form class="form--register">
+             <h2>Please fill the form to create an account :</h2>
             <label for="firstname">First Name</label>
-            <input type="text" placeholder="First Name" id="firstname" />
+            <input
+                class="input--register"
+                type="text"
+                placeholder="First Name"
+                id="firstname"
+            />
 
             <label for="lastname">Last Name</label>
-            <input type="text" placeholder="Last Name" id="lastname" />
+            <input
+                class="input--register"
+                type="text"
+                placeholder="Last Name"
+                id="lastname"
+            />
 
             <label for="dateofbirthday">Date of Birthday</label>
-            <input type="date" id="dateofbirthday" />
+            <input class="input--register" type="date" id="dateofbirthday" />
 
             <label for="username">Username</label>
-            <input type="text" placeholder="Address Email" id="username" />
+            <input
+                class="input--register"
+                type="text"
+                placeholder="Address Email"
+                id="username"
+            />
 
             <label for="password">Password</label>
-            <input type="password" placeholder="Password" id="password" />
-
-            <label for="password">Confirm Password</label>
             <input
+                class="input--register"
                 type="password"
-                placeholder="Confirm Password"
+                placeholder="Password"
                 id="password"
             />
 
-            <button>Log In</button>
+            <label for="password">Confirm Password</label>
+            <input
+                class="input--register"
+                type="password"
+                placeholder="Confirm Password"
+                id="confirm-password"
+            />
+
+            <button class="btn--register">Register</button>
+            <router-link to="/login">I have an account</router-link>
         </form>
     </div>
 </template>
-<script>
-const chooseFile = document.getElementById("choose-file");
-const imgPreview = document.getElementById("img-preview");
-
-chooseFile.addEventListener("change", function () {
-  getImgData();
-});
-
-function getImgData() {
-  const files = chooseFile.files[0];
-  if (files) {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(files);
-    fileReader.addEventListener("load", function () {
-      imgPreview.style.display = "block";
-      imgPreview.innerHTML = '<img src="' + this.result + '" />';
-    });    
-  }
-}
-</script>
+<script></script>
 <style>
-#img-preview {
-    display: none;
-    width: 155px;
-    border: 2px dashed #333;
-    margin-bottom: 20px;
-}
-#img-preview img {
-    width: 100%;
-    height: auto;
-    display: block;
-}
-[type="file"] {
-    height: 0;
-    width: 0;
-    overflow: hidden;
-}
-[type="file"] + label {
-    font-family: sans-serif;
-    background: #f09819;
-    padding: 10px 30px;
-    border: 2px solid #f09819;
-    border-radius: 3px;
-    color: #fff;
-    cursor: pointer;
-    transition: all 0.2s;
-}
-[type="file"] + label:hover {
-    background-color: #23a2f6;
-    color: #fff;
-    border: 2px solid #23a2f6;
-}
-.wrapper--register {
-    overflow: scroll;
-    height: 1000px;
-    position: relative;
-}
 :root {
     --primary-color: rgb(185, 15, 15);
     --secondary-color: rgb(158, 12, 12);
@@ -124,8 +84,13 @@ function getImgData() {
 body {
     background-color: var(--dark-theme);
 }
+.wrapper--register {
+    overflow: scroll;
+    height: 1400px;
+    position: relative;
+}
 .background {
-    width: 430px;
+    width: 50%;
     height: 520px;
     position: absolute;
     transform: translate(-50%, -50%);
@@ -143,13 +108,18 @@ body {
     left: -80px;
     top: -80px;
 }
+.shape:hover {
+    width: 300px;
+    height: 300px;
+    transform: rotate(360deg);
+}
 .shape:last-child {
     background: linear-gradient(to right, #ff512f, #f09819);
     right: -30px;
     bottom: -80px;
 }
-form {
-    width: 400px;
+.form--register {
+    width: 35%;
     background-color: rgba(255, 255, 255, 0.13);
     position: absolute;
     transform: translate(-50%, -50%);
@@ -160,15 +130,12 @@ form {
     border: 2px solid rgba(255, 255, 255, 0.1);
     box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
     padding: 50px 35px;
-}
-form {
-    font-family: "Poppins", sans-serif;
     color: #ffffff;
     letter-spacing: 0.5px;
     outline: none;
     border: none;
 }
-form h3 {
+.form--register h3 {
     font-size: 32px;
     font-weight: 500;
     line-height: 42px;
@@ -181,7 +148,7 @@ label {
     font-size: 16px;
     font-weight: 500;
 }
-input {
+.input--register {
     display: block;
     height: 50px;
     width: 100%;
@@ -195,7 +162,7 @@ input {
 ::placeholder {
     color: #e5e5e5;
 }
-button {
+.btn--register {
     margin-top: 50px;
     width: 100%;
     background-color: #ffffff;
@@ -205,27 +172,5 @@ button {
     font-weight: 600;
     border-radius: 5px;
     cursor: pointer;
-}
-.social {
-    margin-top: 30px;
-    display: flex;
-}
-.social div {
-    background: red;
-    width: 150px;
-    border-radius: 3px;
-    padding: 5px 10px 10px 5px;
-    background-color: rgba(255, 255, 255, 0.27);
-    color: #eaf0fb;
-    text-align: center;
-}
-.social div:hover {
-    background-color: rgba(255, 255, 255, 0.47);
-}
-.social .fb {
-    margin-left: 25px;
-}
-.social i {
-    margin-right: 4px;
 }
 </style>
